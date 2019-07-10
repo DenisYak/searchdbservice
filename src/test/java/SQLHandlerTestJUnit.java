42f3feeff648e2c3703ad2c139441049e457ffea
DenisYak <denis_yak@mail.ru>
2019-07-11 01:01:38 +0300

    mockito test

--- src/test/java/SQLHandlerTestJUnit.java
+++ src/test/java/SQLHandlerTestJUnit.java
@@ -3,7 +3,6 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.sql.*;
 
 public class SQLHandlerTestJUnit {
 
--- src/test/java/SQLHandlerTestMockito.java
+++ src/test/java/SQLHandlerTestMockito.java
@@ -1,30 +1,48 @@
-
-
+import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
 
 public class SQLHandlerTestMockito extends Mockito {
 
     @Mock
-    SQLHandler sqlHandler;
+    private SQLHandler sqlHandler;
 
     @Before
     public void setUp() {
+        MockitoAnnotations.initMocks(this);
         sqlHandler.connectToDB();
         sqlHandler.createTableUsers();
-        sqlHandler.insertSpecificUser("bob", "marley");
-        sqlHandler.insertSpecificUser("rob", "thomas");
-        sqlHandler.insertSpecificUser("james", "hetfield");
+//        sqlHandler.insertSpecificUser("bob", "marley");
+//        sqlHandler.insertSpecificUser("rob", "thomas");
+//        sqlHandler.insertSpecificUser("james", "hetfield");
     }
 
     @Test
-    public void searchByFirstName() {
+    public void searchByFirstNameTest() {
         when(sqlHandler.searchByFirstName("bob")).thenReturn(1);
+        sqlHandler.searchByFirstName("bob");
         verify(sqlHandler).searchByFirstName("bob");
+        Assert.assertTrue(sqlHandler.searchByFirstName("bob") == 1);
     }
 
+    @Test
+    public void updateLastNameByFirstNameTest() {
+        sqlHandler.updateLastNameByFirstName("bob", "dylan");
+        verify(sqlHandler).updateLastNameByFirstName("bob", "dylan");
+        when(sqlHandler.selectLastNameByFirstName("bob")).thenReturn("dylan");
+        String actual = sqlHandler.selectLastNameByFirstName("bob");
+        verify(sqlHandler).selectLastNameByFirstName("bob");
+        String expected = "dylan";
+        Assert.assertEquals(expected, actual);
+    }
 
+    @After
+    public void tearDown() {
+        sqlHandler.disconnectFromDB();
+    }
 }
